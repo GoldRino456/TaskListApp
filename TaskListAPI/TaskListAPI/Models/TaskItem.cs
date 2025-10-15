e7ad09f6024806fd74089221bae051c186055370
Ethan E. <eastwoode375@gmail.com>
2025-10-15 17:00:15 -0500

    Minimal API Init Commit

---
+++ TaskListAPI/TaskListAPI/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskListAPI.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsComplete { get; set; } = false;
+}
